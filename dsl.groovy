2fa1d6913714fa318f609dbf6ee997f953888726
Devopsawsr <133389232+Devopsawsr@users.noreply.github.com>
2024-01-22 20:31:52 +0530

    Create dsl.groovy

---
+++ dsl.groovy
@@ -0,0 +1,22 @@
+multibranchPipelineJob("Reddy-groovy") {
+    branchSources {
+        git {
+            id('Devops')
+            credentialsId('Bithub')
+            remote('https://github.com/Devopsawsr/Docker.git')
+            includes('*')  // Include all branches
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(-1)
+        }
+    }
+    configure {
+        def triggers = it / triggers
+        triggers << 'com.cloudbees.jenkins.plugins.BitBucketMultibranchTrigger' {
+            spec ''
+            overrideUrl('https://github.com/Devopsawsr/Docker.git')
+        }
+    }
+}
