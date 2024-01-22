5cb8c13c6abcc871c743855fa8c9d2734dcc0635
Devopsawsr <133389232+Devopsawsr@users.noreply.github.com>
2024-01-22 20:59:41 +0530

    Update dsl.groovy

--- dsl.groovy
+++ dsl.groovy
@@ -1,4 +1,4 @@
-multibranchPipelineJob("DSL") {
+multibranchPipelineJob('DSL') {
     branchSources {
         git {
             id('Devops')
@@ -10,6 +10,26 @@ multibranchPipelineJob("DSL") {
     orphanedItemStrategy {
         discardOldItems {
             numToKeep(-1)
+        }
+    }
+    configure {
+        def triggers = it / triggers
+        triggers << 'com.cloudbees.jenkins.plugins.GitHubMultibranchTrigger' {
+            spec('')
+            overrideUrl('https://github.com/Devopsawsr/Docker.git')
+        }
+    }
+    configure {
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'jenkins.plugins.git.traits.TagDiscoveryTrait' {
+            strategyId(3)
+        }
+    }
+    configure {
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait' {
+            includes('develop hotfix- dev- cit- sit- uat- pre- prd- release- feature-*')
+            excludes('main') // master branch is not exposed to Jenkins builds...
         }
     }
 }
